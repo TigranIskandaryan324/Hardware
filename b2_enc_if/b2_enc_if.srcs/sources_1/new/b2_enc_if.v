90aef294a3966a3295eac42d04a6deeabcb97fe1
TigranIskandaryan324 <tikiskandaryan5@gmail.com>
2023-10-10 18:17:51 +0400

    Encoder by if.

---
+++ b2_enc_if/b2_enc_if.srcs/sources_1/new/b2_enc_if.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/10/2023 06:16:51 PM
+// Design Name: 
+// Module Name: b2_enc_if
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module b2_enc_if(
+
+    );
+endmodule
